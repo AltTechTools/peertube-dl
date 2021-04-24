f5503c1a15b182b768ff915fbc2c8aeb92f90b2c
fssSGJ <fsstweeter@gmail.com>
2021-04-24 14:34:46 +0200

    updated torrent excample

--- torrenttools/transtorrentadd-custom.sh
+++ torrenttools/transtorrentadd-custom.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+
 [ $(pgrep -x "transmission-da" | grep -c "") -gt 0 ] || (transmission-daemon && notify-send -i process-working "Starting Torrent Daemon ...")
 
 Result=$(transmission-remote -a "$@" -g all -s -w "$PWD" && notify-send -i document-save-as.png "Transmission-Remote:" " - Torrent added")
-notify-send -i document "Transmission-Remote Custom:" " - ${Result}"
+notify-send -i "info" "Transmission-Remote Custom:" " - ${Result}"
